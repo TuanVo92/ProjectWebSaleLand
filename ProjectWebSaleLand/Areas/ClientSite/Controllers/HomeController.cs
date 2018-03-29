6b40d2771d0838afdd573ebc3d85fda9a9d0122c
TuanVo <voquoctuanqn@gmail.com>
2018-03-29 10:03:08 +0700

    [+] add source code

---
+++ ProjectWebSaleLand/Areas/ClientSite/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectWebSaleLand.Areas.ClientSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: ClientSite/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MoreProperties()
+        {
+            return View();
+        }
+
+        public ActionResult PropertiesDetail()
+        {
+            return View();
+        }
+    }
+}
